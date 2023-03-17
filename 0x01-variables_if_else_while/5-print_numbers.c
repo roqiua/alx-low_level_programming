4cb7ef7414d23842017d4d4233ca5a73d8754937
roqiua <roqiuamohammad13@.com>
2023-03-17 07:58:47 -0700

    5-print_numbers.c commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print digit number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
