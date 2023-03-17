9112cbb47255f2a332b30fad40f64c381c024bc0
roqiua <roqiuamohammad13@.com>
2023-03-17 10:44:47 -0700

    advanced commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,10 +12,10 @@ int main(void)
 	{
 		for (m = n + 1; m <= 99; m++)
 		{
-			putchar((n % 10) + '0');
+			putchar((n / 10) + '0');
 			putchar((n % 10) + '0');
 			putchar(' ');
-			putchar((m % 10) + '0');
+			putchar((m / 10) + '0');
 			putchar((m % 10) + '0');
 			if (n == 98 && m == 99)
 				continue;
