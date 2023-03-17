d44d822eb9e492f5e1b1a330e404c96f857d1566
roqiua <roqiuamohammad13@.com>
2023-03-17 07:36:07 -0700

    3-print_alphabets.c commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - alphabet game
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	n = 'a';
+	while (n != '{')
+	{
+		putchar(n);
+		n++;
+	}
+	n = 'A';
+	while (n != '[')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
