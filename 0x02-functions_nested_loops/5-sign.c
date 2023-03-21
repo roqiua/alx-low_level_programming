f353f8e08aeea33331989385f314f72a1d000f6b
roqiua <roqiuamohammad13@.com>
2023-03-21 09:13:36 -0700

    5-sign.c commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,20 +1,26 @@
-
-
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - print sign of num
+ * @n: input
+ *
+ * Return: return 1 or -1 or 0
+ */
 int print_sign(int n)
 {
-	if(n < 0)
+	if (n < 0)
 	{
-		printf('-');
+		_putchar('-');
 		return (-1);
 	}
-	else if(n > 0)
+	else if (n > 0)
 	{
-		printf('+');
+		_putchar('+');
 		return (1);
 	}
 	else
 	{
-		prinrf(0);
+		_putchar(0);
 		return (0);
 	}
 }
