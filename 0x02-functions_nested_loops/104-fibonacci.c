5d45bbc92511b5e79b7d8c910ff5628890ea3078
roqiua <roqiuamohammad13@.com>
2023-03-21 12:20:49 -0700

    104-fibonacci.c commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,11 +5,11 @@
  * Return: return 0
  */
 
-int main(void)    
+int main(void)
 {
 	int i, n1 = 0, n2 = 1, n3 = 0;
 
-	for (i = 0; i <=98; i++)
+	for (i = 2; i <= 98; i++)
 	{
 		n3 = (n1 + n2);
 		printf("%d", n3);
