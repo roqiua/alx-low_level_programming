d3fd5550bdd09f32d3fabe5c49da0f2112396483
roqiua <roqiuamohammad13@.com>
2023-03-21 10:46:16 -0700

    8-24_hours.c commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,32 @@
-
-
-
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * jack_bauer - function to print time
+ *
+ * Return: no return
+ */
 
 void jack_bauer(void)
 {
-	int i;
-	
-	for( i = 0; i <= 2; i++)
+	int i, j, k, h;
+
+	for (i = 0 ; i <= 2 ; i++)
 	{
-		for(j = 0; j 
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (h = 0; h <= 9; h++)
+				{
+					printf("%d%d:%d%d\n", i, j, k, h);
+					if (i == 2 && j == 3 && k == 5 && h == 9)
+					{
+						exit(0);
+					}
+				}
+			}
+		}
+	}
+
+}
