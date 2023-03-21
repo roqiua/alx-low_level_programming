ef18b241f0b414dcf3b89e072577ca4d73afcc00
roqiua <roqiuamohammad13@.com>
2023-03-21 08:29:58 -0700

    2-print_alphabet_x10.c commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,22 @@
-#include 
+#include <stdio.h>
+#include "main.h"
 /**
+ * print_alphabet_x10 - print alphabet ten times
  *
+ * Return: no return
  */
 
 void print_alphabet_x10(void)
 {
 	char c;
 	int i;
-	for(i = 0; i <= 10; i++){
-		for(c = a; c <= 'z'; c++)
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
