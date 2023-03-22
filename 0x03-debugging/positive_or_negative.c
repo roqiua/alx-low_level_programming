a6fd8692347ba1e48d2653b60eaf7381f20ae33c
roqiua <roqiuamohammad13@.com>
2023-03-22 16:48:55 -0700

    Multiple mains commit

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0 (Success)
+ */
+int positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+	return (0);
+}
+
+
+
