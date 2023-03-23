3f64d9b7ee163941968bfdbc2d829aa97492f0b0
roqiua <roqiuamohammad13@.com>
2023-03-23 03:35:01 -0700

    5-more_numbers.c commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - print num from 0 to 9
+ *
+ * Return: no return
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14 ; j++)	
+		{
+			if (j > 9)
+			{
+				_putchar(j / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+
+	_putchar('\n');
+}
