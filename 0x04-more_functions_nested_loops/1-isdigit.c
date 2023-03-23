f91aa93907edff9bc6f054a871764d65399e9a64
roqiua <roqiuamohammad13@.com>
2023-03-23 02:50:42 -0700

    1-isdigit.c commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isdigit - function
+ * @c: input
+ *
+ * Return: return 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
