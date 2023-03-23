3924f2ade3fa909ebc6a62ff27865f211c49d994
roqiua <roqiuamohammad13@.com>
2023-03-23 06:42:01 -0700

    advanced commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,15 +10,20 @@ int main(void)
 {
 	long num = 612852475143;
 	int i;
+	int result = 0;
 
 	for (i = 2; num != 1; i++)
 	{
 		while (num % i == 0)
 		{
+			if (result < i)
+			{
+				result = i;
+			}
 			num = num / i;
 		}
 	}
-	printf("%d", i);
+	printf("%d", result);
 	printf("\n");
 	return (0);
 }
