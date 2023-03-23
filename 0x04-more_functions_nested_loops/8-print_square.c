47044c05d5f8ddc3e706b8ba451bf9b0be48da30
roqiua <roqiuamohammad13@.com>
2023-03-23 04:39:12 -0700

    8-print_square.c commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_square - function
+ * @size: input
+ *
+ * Return: no return
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+}
