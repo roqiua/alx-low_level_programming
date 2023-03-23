9a8a7b24cd33d432dfd6535488b30816c8b726d9
roqiua <roqiuamohammad13@.com>
2023-03-23 02:26:57 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/** _isupper - fun for is upper
+ * @c: input
+ *
+ * Return: return 1 or 0
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+tasks for 0x04. C - More functions, more nested loops
