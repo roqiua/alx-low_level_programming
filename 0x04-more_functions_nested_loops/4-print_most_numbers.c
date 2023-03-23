d991c800a1951866af8f366a2d839c7e564c6e68
roqiua <roqiuamohammad13@.com>
2023-03-23 03:13:55 -0700

    4-print_most_numbers.c commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - print num from 0 to 9
+ *
+ * Return: no return
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
